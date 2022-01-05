7379982a8391a2106182b259f5930c016f2c8e77
TimVandergoten <tim.vandergoten@student.ehb.be>
2022-01-05 12:37:12 +0100

    search start

--- app/src/main/java/com/timVandergoten/android/MainActivity.kt
+++ app/src/main/java/com/timVandergoten/android/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.timVandergoten.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar =
+            findViewById<Toolbar>(R.id.toolbar2)
+        setSupportActionBar(toolbar)
+
     }
 }
